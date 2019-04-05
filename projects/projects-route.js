c429fe5d4f922fa377d56a977e33bc0113830be8
paintedlbird7 <rosaperezlarios@gmail.com>
2019-04-05 09:37:24 -0700

    posts working

--- projects/projects-route.js
+++ projects/projects-route.js
@@ -40,18 +40,18 @@ router.get('/', async (req, res) => {
   
   router.get('/:id/projects', async (req, res) => {
     try {
-      const projects = await Projects.findProjectMessages(req.params.id);
+      const projects = await Projects.findPostMessages(req.params.id);
   
       if (projects.length > 0) {
         res.status(200).json(projects);
       } else {
-        res.status(404).json({ message: 'No messages for this project' });
+        res.status(404).json({ message: 'No messages for this post' });
       }
     } catch (error) {
       // log error to database
       console.log(error);
       res.status(500).json({
-        message: 'Error retrieving the messages for this project',
+        message: 'Error retrieving the messages for this post',
       });
     }
   });
@@ -61,9 +61,9 @@ router.get('/', async (req, res) => {
     try {
       const count = await Projects.remove(req.params.id);
       if (count > 0) {
-        res.status(200).json({ message: 'The project has been nuked' });
+        res.status(200).json({ message: 'The post has been nuked' });
       } else {
-        res.status(404).json({ message: 'The project could not be found' });
+        res.status(404).json({ message: 'The post could not be found' });
       }
     } catch (error) {
       // log error to database
@@ -77,54 +77,52 @@ router.get('/', async (req, res) => {
   router.put('/:id', async (req, res) => {
     // const changes = req.body;
     try {
-      const project = await Projects.update(req.params.id, req.body);
+      const post = await Projects.update(req.params.id, req.body);
       if (project) {
         res.status(200).json(project);
       } else {
-        res.status(404).json({ message: 'The project could not be found' });
+        res.status(404).json({ message: 'The post could not be found' });
       }
     } catch (error) {
       // log error to database
       console.log(error);
       res.status(500).json({
-        message: 'Error updating the project',
+        message: 'Error updating the post',
       });
     }
   });
   
 
-  // router.post("/", async (req, res) => {
-  //   console.log("project", req.body);
-  //   try {
-  //     const projectData = req.body;
-  //     const projectId = await Projects.insert(projectData);
-  //     const projects = await Projects.findById(projectId.id);
-  //     res.status(201).json(project);
-  //   } catch (error) {
-  //     let message = "There was an error while saving the project to the database";
+  router.post("/", async (req, res) => {
+    console.log("project", req.body);
+    try {
+      const projectData = req.body;
+      const projectId = await Projects.insert(projectData);
+      const projects = await Projects.findById(projectId.id);
+      res.status(201).json(post);
+    } catch (error) {
+      let message = "There was an error while saving the project to the database";
   
-  //     if (error.errno === 19) {
-  //       message = "please provide both the title and the contents";
-  //     }
-  //     res.status(500).json({ message: message, error });
-  //   }
-  // });
-
-
-  router.post('/:id/projects', async (req, res) => {
-    // const named projectBody = req.body
-      const projectInfo = { ...req.body, project_id: req.params.id }
-      try {
-          const project = await Projects.addProject(projectInfo)
-          res.status(201).json(project)
-      } catch(error) {
-            // log error to database
-          console.log(error)
-          res.status(500).json({
-              message: 'Errrrroorrrrrrrr'
-          })
+      if (error.errno === 19) {
+        message = "please provide both the title and the contents";
       }
-  })
+      res.status(500).json({ message: message, error });
+    }
+  });
+  // router.post('/:id/projects', async (req, res) => {
+  //   // const named postBody = req.body
+  //     const postInfo = { ...req.body, post_id: req.params.id }
+  //     try {
+  //         const post = await Projects.addPost(postInfo)
+  //         res.status(201).json(post)
+  //     } catch(error) {
+  //           // log error to database
+  //         console.log(error)
+  //         res.status(500).json({
+  //             message: 'Errrrroorrrrrrrr'
+  //         })
+  //     }
+  // })
         
 
 
