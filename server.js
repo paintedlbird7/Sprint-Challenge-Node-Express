76e2c5c142242508d73473d1b5744e38f3fa44b7
paintedlbird7 <rosaperezlarios@gmail.com>
2019-04-05 08:19:43 -0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+//import router
+
+const projectsRouter = require('./projects/projects-route.js')
+const server = express();
+
+server.use(express.json());
+
+server.use('/api/projects', projectsRouter) 
+// Delegates requests to /api/posts to the router.
+
+// const Posts = require('./posts/posts-model.js');
+
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Hello World</h>
+  `);
+});
+
+
+module.exports = server // CommonJS way of exporting out of a module.
+
+// Same as export default server
